bcbec0ac46c5829280616ff6b8ca3086e58d8121
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-08-30 09:26:13 -0400

    minor channges made

--- src/components/GetImageForm.js
+++ src/components/GetImageForm.js
@@ -25,7 +25,7 @@ class GetImageForm extends Component {
   }
 
 
-  fetchRoverImage(rover, sol, camera){
+  fetchRoverImage(){
   let cam = this.state.camera;
   let rove = this.state.rover;
   let num = this.state.sol;
@@ -33,7 +33,7 @@ class GetImageForm extends Component {
   let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rove}/photos?sol=${num}&camera=${cam}&api_key=${API_KEY}`;
 
   fetch(imageUrl)
-  .then((response) => response.json())
+  .then(response => response.json())
   .then((data) => {
     this.setState({images: data.photos})
   })
@@ -63,19 +63,19 @@ class GetImageForm extends Component {
     return (
       <div>
         <label htmlFor="rover">Rover</label>
-        <select onChange={this.handleRover} id="rover" value={this.state.value}>
+        <select onChange={this.handleRover} id="rover" value={this.state.rover}>
           <option value="Curiosity">Curiosity</option>
           <option value="Opportunity">Opportunity</option>
           <option value="Spirit">Spirt</option>
         </select>
         <label htmlFor="camera">Camera Type</label>
-        <select onChange={this.handleCamera} id="rover" value={this.state.value}>
+        <select onChange={this.handleCamera} id="camera" value={this.state.camera}>
           <option value="fhaz">FHAZ (Front Hazard)</option>
           <option value="rhaz">RHAZ (Rear Hazard)</option>
           <option value="navcam">NAVCAM (Navigation Cam)</option>
         </select>
         <label htmlFor="sol">Martian Sol: 1000-2000</label>
-        <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.value}/>
+        <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.sol}/>
         <GetImageButton handleClick={this.fetchRoverImage}/>
         <ImageDisplay images={this.state.images}/>
       </div>
--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 class ImageDisplay extends Component {
   render() {
-    let key = 0;
     let photos =this.props.images
     let images = photos.map((photo)=>{
-      key++;
       return (
-          <ul key ={photo.id}>
-            <li>
+          <ul>
+            <li key ={photo.id}>
               <img src={photo.img_src}/>
             </li>
           </ul>
